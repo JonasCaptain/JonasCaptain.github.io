4088194dfe957a68b3c8a2708f5873be7f99f584
v_fanzhigao <v_fanzhigao@tencent.com>
2023-02-16 20:49:48 +0800

    inital project

---
+++ src/utils/Posts.ts
@@ -0,0 +1,14 @@
+import type {
+  IFrontmatter,
+  MarkdownInstance,
+} from 'astro-boilerplate-components';
+
+export const sortByDate = (posts: MarkdownInstance<IFrontmatter>[]) => {
+  const result =  posts.sort(
+    (a, b) =>
+      new Date(b.frontmatter.pubDate).valueOf() -
+      new Date(a.frontmatter.pubDate).valueOf()
+  );
+  return result;
+  
+};
