16bb998879a31a94abfdb8068b18e0eccc736a66
v_fanzhigao <v_fanzhigao@tencent.com>
2023-02-21 09:48:27 +0800

    替换astro模板代码

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site_name: 'Pure Blog',
-  title: 'Pure Blog',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Jonas',
-  locale_region: 'zh-cn',
-  locale: 'zh',
-};
